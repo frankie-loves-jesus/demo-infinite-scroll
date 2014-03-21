766f2afdbe746ae5c62a6e601d0a03dcd3a481ae
Frankie <orangeprince@hushmail.com>
2014-03-21 02:53:10 +0100

    rails new demo-infinite-scroll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoInfiniteScroll::Application.config.secret_key_base = 'ee4a4ddf02b5997ecb4dcb1ebd16c67f74207820b5c9186b87a853d6408e5a3789bc4f8a89b7ebca5adb54b05cd6e512f24f6844595c71f3524877a2a5cfaf19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoInfiniteScroll::Application.config.session_store :cookie_store, key: '_demo-infinite-scroll_session'
